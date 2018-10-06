570bbc13c9bc533211f8fcdcafbc33228172e65a
kylep <kjp44@njit.edu>
2018-10-05 20:39:08 -0400

    Bug Fix: Fixed issue with Bootstrap.

--- public/index.php
+++ public/index.php
@@ -85,7 +85,11 @@ class csv {
 
 class system {
     public static function printPage($tableHTML) {
-        echo $tableHTML;
+        $pageHTML = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">';
+        $pageHTML .= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">';
+        $pageHTML .= '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>';
+        $pageHTML .= $tableHTML;
+        echo $pageHTML;
     }
 }
 

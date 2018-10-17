561458cfe2aa64c6a8803094d056c9c927f301e6
kylep <kjp44@njit.edu>
2018-10-17 00:21:59 -0400

    Task: Refactored code to better conform to DRY and flow better visually.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: kwilliams
+ * User: Kyle Payne
  * Date: 9/26/18
  * Time: 6:42 PM
  */
@@ -9,57 +9,12 @@ main::start('example.csv');
 
 class main {
     static public function start($fileName) {
-        $rawRecords = csv::readCSV($fileName);
-        $properties = $rawRecords[0];
-        array_splice($rawRecords,0,1);
-        $recordsAsObjects = (csv::convertRecordsToObjects($properties, $rawRecords));
+        $rawData = csv::readCSV($fileName);
+        $properties = csv::getProperties($rawData);
+        $records = csv::getRecords($rawData);
+        $recordsAsObjects = (csv::convertRecordsToObjects($properties, $records));
         $tableHTML = html::createTable($properties, $recordsAsObjects);
         system::printPage($tableHTML);
-    }
-}
-
-class html {
-    static public function createTable($properties, $recordsAsObjects) {
-        $html = '<table class="table table-striped">';
-        $html .= html::createTableHeader($properties);
-        foreach($recordsAsObjects as $recordAsObject){
-            $html .= html::createTableRow($properties, $recordAsObject);
-        }
-        $html .= '</table>';
-        return $html;
-    }
-    static public function createTableHeader($properties){
-        $html = "<thead>";
-        $html .= "<tr>";
-        foreach($properties as $property){
-            $html .= html::createTableHeaderData($property);
-        }
-        $html .= "</tr>";
-        $html .= "</thead>";
-        return $html;
-    }
-    static public function createTableHeaderData($property){
-        $html = '<th scope="col">';
-        $html .= $property;
-        $html .= "</th>";
-        return $html;
-    }
-    static public function createTableRow($properties, $recordAsObject) {
-            $html = '<tr>';
-            $html .= '<th scope="row">';
-            $html .= $recordAsObject->{$properties[0]};
-            $html .= '</th>';
-            for($i = 1; $i < count($properties); $i++){
-                $html .= html::createTableRowData($recordAsObject->{$properties[$i]});
-            }
-            $html .= '</tr>';
-        return $html;
-    }
-    static public function createTableRowData($data) {
-        $html = '<td>';
-        $html .= $data;
-        $html .= '</td>';
-        return $html;
     }
 }
 
@@ -74,12 +29,86 @@ class csv {
         fclose($file);
         return($rawRecords);
     }
+    public static function getProperties($rawData){
+        $properties = $rawData[0];
+        return $properties;
+    }
+    public static function getRecords($rawData){
+        $records = array();
+        for($i = 1; $i < count($rawData); $i++){
+            $records[] = $rawData[$i];
+        }
+        return $records;
+    }
     public static function convertRecordsToObjects($properties, $rawRecords){
         $records = array();
         foreach($rawRecords as $rawRecord) {
-            $records[] = RecordFactory::create($properties, $rawRecord);
+            $records[] = recordFactory::create($properties, $rawRecord);
         }
         return $records;
+    }
+}
+
+class recordFactory
+{
+    public static function create($properties, $rawRecord)
+    {
+
+        return new record($properties, $rawRecord);
+    }
+}
+
+class record
+{
+
+    public function __construct($properties, $rawRecord)
+    {
+        for($i = 0; $i<count($properties); $i++) {
+            $this->{$properties[$i]} = $rawRecord[$i];
+        }
+    }
+}
+
+class html {
+    static public function createTable($properties, $recordsAsObjects) {
+        array_unshift($recordsAsObjects, $properties);
+        $count = 0;
+        $html = '<table class="table table-striped">';
+        foreach($recordsAsObjects as $recordAsObject){
+            $dataHolder = '';
+            if($count == 0){
+                foreach($recordAsObject as $data){
+                    $dataHolder .= html::createTableHeader($data);
+                }
+                $count++;
+            }
+            else{
+                foreach($recordAsObject as $data){
+                    $dataHolder .= html::createTableData($data);
+                }
+            }
+            $html .= html::createTableRow($dataHolder);
+        }
+        $html .= '</table>';
+        return $html;
+    }
+    static public function createTableHeader($header){
+        $html = '<th scope="col">';
+        $html .= $header;
+        $html .= "</th>";
+        return $html;
+    }
+    static public function createTableData($data) {
+        $html = '<td>';
+        $html .= $data;
+        $html .= '</td>';
+        return $html;
+    }
+    static public function createTableRow($row) {
+            $html = '<tr>';
+            $html .= $row;
+            $html .= '</tr>';
+        return $html;
     }
 }
 
@@ -90,25 +119,5 @@ class system {
         $pageHTML .= '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>';
         $pageHTML .= $tableHTML;
         echo $pageHTML;
-    }
-}
-
-class Record
-{
-
-    public function __construct($properties, $rawRecord)
-    {
-        for($i = 0; $i<count($properties); $i++) {
-            $this->{$properties[$i]} = $rawRecord[$i];
-        }
-    }
- }
-
-class RecordFactory
-{
-    public static function create($properties, $rawRecord)
-    {
-
-        return new Record($properties, $rawRecord);
     }
 }

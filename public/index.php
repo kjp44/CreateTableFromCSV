123223c3fa0ef50fec9a004e2ea3f0e2b0163a00
kjp44 <Sabbath51>
2018-10-21 01:35:00 -0400

    Refactored code to better conform to SOLID.

--- public/index.php
+++ public/index.php
@@ -9,16 +9,21 @@ main::start('example.csv');
 
 class main {
     static public function start($fileName) {
-        $rawData = csv::readCSV($fileName);
-        $properties = csv::getProperties($rawData);
-        $records = csv::getRecords($rawData);
-        $recordsAsObjects = (csv::convertRecordsToObjects($properties, $records));
-        $tableHTML = html::createTable($properties, $recordsAsObjects);
+        $recordsAsObjects = csv::createRecordsAsObjects($fileName);
+        $tableHTML = html::createBootstrapTable($recordsAsObjects);
         system::printPage($tableHTML);
     }
 }
 
 class csv {
+    public static function createRecordsAsObjects($fileName){
+        $rawRecords = csv::readCSV($fileName);
+        $records = array($rawRecords[0]);
+        for($i = 1; $i < count($rawRecords); $i++) {
+            $records[] = recordFactory::create($records[0], $rawRecords[$i]);
+        }
+        return $records;
+    }
     public static function readCSV($fileName)
     {
         $rawRecords = array();
@@ -29,38 +34,18 @@ class csv {
         fclose($file);
         return($rawRecords);
     }
-    public static function getProperties($rawData){
-        $properties = $rawData[0];
-        return $properties;
-    }
-    public static function getRecords($rawData){
-        $records = array();
-        for($i = 1; $i < count($rawData); $i++){
-            $records[] = $rawData[$i];
-        }
-        return $records;
-    }
-    public static function convertRecordsToObjects($properties, $rawRecords){
-        $records = array();
-        foreach($rawRecords as $rawRecord) {
-            $records[] = recordFactory::create($properties, $rawRecord);
-        }
-        return $records;
-    }
 }
 
 class recordFactory
 {
     public static function create($properties, $rawRecord)
     {
-
         return new record($properties, $rawRecord);
     }
 }
 
 class record
 {
-
     public function __construct($properties, $rawRecord)
     {
         for($i = 0; $i<count($properties); $i++) {
@@ -70,54 +55,100 @@ class record
 }
 
 class html {
-    static public function createTable($properties, $recordsAsObjects) {
-        array_unshift($recordsAsObjects, $properties);
-        $count = 0;
-        $html = '<table class="table table-striped">';
-        foreach($recordsAsObjects as $recordAsObject){
-            $dataHolder = '';
-            if($count == 0){
-                foreach($recordAsObject as $data){
-                    $dataHolder .= html::createTableHeader($data);
-                }
-                $count++;
-            }
-            else{
-                foreach($recordAsObject as $data){
-                    $dataHolder .= html::createTableData($data);
-                }
-            }
-            $html .= html::createTableRow($dataHolder);
+    static public function createBootstrapTable($recordsAsObjects){
+        $html = html::addBootstrap();
+        $html .= html::createTable($recordsAsObjects);
+        return $html;
+    }
+    static public function createTable($recordsAsObjects){
+        $table = html::createTableHead($recordsAsObjects[0]);
+        $table .= html::createTableBody($recordsAsObjects);
+        $table = html::addTableTag($table);
+        return $table;
+    }
+    static public function createTableHead($properties){
+        $tableHeadRow = html::createTableHeadRow($properties);
+        $tableHead = html::addTHeadTag($tableHeadRow);
+        return $tableHead;
+    }
+    static public function createTableHeadRow($properties){
+        $row = html::addTHTagsToRow($properties);
+        $tableHeadRow = html::addTRTag($row);
+        return $tableHeadRow;
+    }
+    static public function addTHTagsToRow($row){
+        $html = '';
+        foreach($row as $data){
+            $html .= html::addTHTag($data);
         }
+        return $html;
+    }
+    static public function createTableBody($recordsAsObjects){
+        $tableBodyRows = html::createTableBodyRows($recordsAsObjects);
+        $tableBody = html::addTBodyTag($tableBodyRows);
+        return $tableBody;
+    }
+    static public function createTableBodyRows($recordsAsObjects){
+        $tableBodyRows = '';
+        for($i = 1;$i < count($recordsAsObjects); $i++){
+            $row = html::addTDTagsToRow($recordsAsObjects[$i]);
+            $tableBodyRows .= html::addTRTag($row);
+        }
+        return $tableBodyRows;
+    }
+    static public function addTDTagsToRow($row){
+        $html = '';
+        foreach($row as $data){
+            $html .= html::addTDTag($data);
+        }
+        return $html;
+    }
+    static public function addTableTag($data){
+        $html = '<table class="table table-striped">';
+        $html .= $data;
         $html .= '</table>';
         return $html;
     }
-    static public function createTableHeader($header){
-        $html = '<th scope="col">';
+    static public function addTHeadTag($data){
+        $html = $html = '<thead>';
+        $html .= $data;
+        $html .= '</thead>';
+        return $html;
+    }
+    static public function addTBodyTag($data){
+        $html = $html = '<tbody>';
+        $html .= $data;
+        $html .= '</tbody>';
+        return $html;
+    }
+    static public function addTRTag($row) {
+        $html = '<tr>';
+        $html .= $row;
+        $html .= '</tr>';
+        return $html;
+    }
+    static public function addTHTag($header){
+        $html = '<th>';
         $html .= $header;
         $html .= "</th>";
         return $html;
     }
-    static public function createTableData($data) {
+    static public function addTDTag($data) {
         $html = '<td>';
         $html .= $data;
         $html .= '</td>';
         return $html;
     }
-    static public function createTableRow($row) {
-            $html = '<tr>';
-            $html .= $row;
-            $html .= '</tr>';
+    static public function addBootstrap(){
+        $html = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">';
+        $html .= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">';
+        $html .= '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>';
         return $html;
     }
 }
 
 class system {
-    public static function printPage($tableHTML) {
-        $pageHTML = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">';
-        $pageHTML .= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">';
-        $pageHTML .= '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>';
-        $pageHTML .= $tableHTML;
-        echo $pageHTML;
+    public static function printPage($html) {
+        echo $html;
     }
 }
